73aab7d617798de1b08ccf0c43f65eb7503d3b93
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-03-22 10:33:54 +0100

    Create Chrun02.py

---
+++ Chrun02.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+sns.set(style="white")
